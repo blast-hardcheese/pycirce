b4d2d193d6ae8e377fba4502aa60a875612c3b31
Devon Stewart <blast@hardchee.se>
2024-02-24 19:11:50 +0000

    Reflowing directory structure

---
+++ tests/test_pycirce.py
@@ -0,0 +1,43 @@
+import unittest
+
+from pycirce import decode_list
+from pycirce import decode_object
+from dataclasses import dataclass
+
+
+class DecodeListTestCase(unittest.TestCase):
+    def test_decode_list(self):
+        decode_increment = decode_list(int)
+        assert decode_increment(["1", "2", "3"]) == [1, 2, 3], "Should decode integers"
+
+
+class DecodeObjectTestCase(unittest.TestCase):
+    @dataclass
+    class Person:
+        name: str
+        age: int
+
+    def test_decode_object(self):
+        Person = DecodeObjectTestCase.Person
+
+        decode_person = decode_object(Person)(age=int)
+        decoded_person = decode_person({"name": "Alice", "age": "30"})
+        assert decoded_person == Person(
+            name="Alice", age=30
+        ), "Age should be converted to int"
+
+    def test_decode_list_of_persons(self):
+        Person = DecodeObjectTestCase.Person
+
+        decode_list_of_persons = decode_list(decode_object(Person)(age=int))
+        decoded_people = decode_list_of_persons(
+            [{"name": "Alice", "age": "30"}, {"name": "Bob", "age": "25"}]
+        )
+        assert decoded_people == [
+            Person(name="Alice", age=30),
+            Person(name="Bob", age=25),
+        ], "Should decode a list of Person objects with ages converted to int"
+
+
+if __name__ == "__main__":
+    unittest.main()
