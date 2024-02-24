0764f9e519bb2dcb835b2654ff88d2f8562ba400
Devon Stewart <blast@hardchee.se>
2024-02-24 20:06:01 +0000

    Custom DecoderException

--- src/pycirce.py
+++ src/pycirce.py
@@ -4,11 +4,19 @@ from typing import Any, TypeVar
 A = TypeVar("A")
 
 
+class DecoderException(Exception):
+    """An error representing any decoder failures"""
+
+
 def decode_list(decoder: Callable[[Any], A]) -> Callable[[list[Any]], list[A]]:
     """Successively apply decoder over an entire array"""
+    __name__ = f"decode_list({decoder.__name__})"  # pylint: disable=W0622
 
     def func(xs):
-        return [decoder(x) for x in xs]
+        try:
+            return [decoder(x) for x in xs]
+        except Exception as e:
+            raise DecoderException(f"Error while applying {__name__}: {e}") from e
 
     func.__name__ = "decode_list({})".format(decoder.__name__)
     return func
@@ -36,7 +44,7 @@ def decode_object(decoder: Callable[..., A]) -> Callable[..., Callable[..., A]]:
                     **{k: fields[k](v) if k in fields else v for k, v in obj.items()}
                 )
             except Exception as e:
-                raise Exception("Error while applying {}: {}".format(__name__, e))
+                raise DecoderException(f"Error while applying {__name__}: {e}") from e
 
         func.__name__ = __name__
         return func
